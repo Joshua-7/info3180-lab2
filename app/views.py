6224f47478a350092cfcef8f4106b66daece68d1
Errol Green <egreen3000@yahoo.com>
2022-02-13 22:19:55 -0500

    Updated css

--- app/views.py
+++ app/views.py
@@ -7,6 +7,7 @@ This file creates your application.
 
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+import datetime
 
 
 ###
@@ -29,6 +30,8 @@ def about():
 @app.route('/profile')
 def profile():
     return render_template('profile.html')
+
+    
 ###
 # The functions below should be applicable to all Flask apps.
 ###
@@ -57,6 +60,10 @@ def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
 
+
+def format_date_joined(d, m, y):
+    date_joined = datetime.date(int(y),int(m),int(d))
+    return ("Joined " + date_joined.strftime("%B , %Y"))
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port="8080")
